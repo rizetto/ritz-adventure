d86e9c0480d988a620e177e3429fa07e7b0318d6
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-10 08:43:56 +0800

    add printMenu function

---
+++ src/Printer.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Printer {
+    private static BufferedReader fileReader;
+
+    public static void printMenu() {
+        try {
+            fileReader = new BufferedReader(new FileReader("./src/res/banner.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String line = fileReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = fileReader.readLine();
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
