7d16751f0efdf92fdfded73e835bbe2af97600ac
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-10 09:18:15 +0800

    test dialog creation and display

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import story.Story;
+import utils.Printer;
+
 public class Main {
     public static void main(String[] args) {
+        Story story = new Story();
+        story.createDialogs();
+
         Printer.printMenu();
     }
 }
