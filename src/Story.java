706a42f6de560dac22a8b08e02a8c55e58356839
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-14 11:47:29 +0800

    add Story.java

---
+++ src/Story.java
@@ -0,0 +1,13 @@
+import utils.Printer;
+
+public class Story {
+    private Printer printer;
+
+    public Story(Printer printer) {
+        this.printer = printer;
+    }
+
+    public void showMenu() {
+
+    }
+}
