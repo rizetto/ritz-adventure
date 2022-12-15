ea27d1946c7ef64792b29a31ee35b4e18579cc85
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 12:06:03 +0800

    add CharacterDatabase as dependency in story

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import models.CharacterDatabase;
 import utils.Dialog;
 import utils.Printer;
 
 public class Main {
     public static void main(String[] args) {
-        Story story = new Story(new Dialog());
-        story.showMenu();
+        Story story = new Story(new Dialog(), new CharacterDatabase());
+        story.startGame();
     }
 }
