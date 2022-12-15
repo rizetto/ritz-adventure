59fdabc08a015e6e34c9306af129eac68a89b526
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 21:52:37 +0800

    add jopay path

--- src/story/Audio.java
+++ src/story/Audio.java
@@ -3,5 +3,6 @@ package story;
 public class Audio {
     public final static String TITLE = "./src/res/audio/title_theme.wav";
     public final static String ADVENTURE = "./src/res/audio/the_journey_begins.wav";
+    public final static String JOPAY = "./src/res/audio/jopay.wav";
     public final static String SELECT = "./src/res/audio/select.wav";
 }
