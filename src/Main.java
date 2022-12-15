9ecc2a8ffed2947fac24fbe5b8ed7a160d64be2d
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 11:18:05 +0800

    add main class

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import utils.Printer;
 
 public class Main {
     public static void main(String[] args) {
-        Printer printer = new Dialog();
-        printer.printFromFile("./src/res/banner.txt");
-        printer.printCountdown(3, "\nStarting");
-        printer.clearScreen();
-        printer.printFromFile("./src/res/menu.txt");
+        Story story = new Story(new Dialog());
+        story.showMenu();
     }
 }
