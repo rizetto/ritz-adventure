9304640b4a4d5b332881dad09e85e0b0af774d16
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 16:19:23 +0800

    add paths to sound fx

--- src/story/Audio.java
+++ src/story/Audio.java
@@ -1,5 +1,7 @@
 package story;
 
 public class Audio {
-    public final static String sevenEleven = "./src/res/audio/the_journey_begins.wav";
+    public final static String TITLE = "./src/res/audio/title_theme.wav";
+    public final static String ADVENTURE = "./src/res/audio/the_journey_begins.wav";
+    public final static String SELECT = "./src/res/audio/select.wav";
 }
