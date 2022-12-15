133f2722399cf7dabf597052d78bcc85316abbe7
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 16:18:52 +0800

    add throwable exceptions

--- src/utils/Playable.java
+++ src/utils/Playable.java
@@ -1,5 +1,9 @@
 package utils;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
 public interface Playable {
-    void startGame();
+    void startGame() throws UnsupportedAudioFileException, LineUnavailableException, IOException;
 }
