91c8c251617aceb1ab287d003dc70153557e3254
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 11:13:41 +0800

    add Console.java class to take user input

---
+++ src/utils/Console.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Console {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static int getIntegerInput(String message) {
+        System.out.print(message);
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input!");
+        }
+
+        return 0;
+    }
+
+    public static String getStringInput(String message) {
+        System.out.print(message);
+        return scanner.nextLine();
+    }
+
+    public static void clearScreen() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println();
+        }
+    }
+}
