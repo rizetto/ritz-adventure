8f5f1713177fb4501466625a7edba87e41723474
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 15:28:37 +0800

    add sound dependency in story

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import models.CharacterDatabase;
+import story.BackgroundMusic;
 import utils.Dialog;
 
 public class Main {
     public static void main(String[] args) {
-        Story story = new Story(new Dialog(), new CharacterDatabase());
+        Story story = new Story(
+                new Dialog(),
+                new CharacterDatabase(),
+                new BackgroundMusic()
+        );
         story.startGame();
     }
 }
