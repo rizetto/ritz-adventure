903fc59de3901511349a8d3606bf78973a6616dc
Rizzeto <ritzvinxentculanag@gmail.com>
2022-12-15 12:06:44 +0800

    add simple game loop and character mode

--- src/Story.java
+++ src/Story.java
@@ -1,19 +1,43 @@
+import models.Character;
 import story.Dialogs;
 import story.StoryMode;
 import utils.Console;
+import utils.Database;
 import utils.Printer;
+
+import java.util.List;
+import java.util.spi.AbstractResourceBundleProvider;
 
 public class Story {
     private Printer printer;
+    private Database<Character> database;
     private StoryMode storyMode;
 
-    public Story(Printer printer) {
+    public Story(Printer printer, Database<Character> database) {
         this.printer = printer;
+        this.database = database;
     }
 
-    public void showMenu() {
+    public void startGame() {
+        showLoadingScreen();
+
+        while (true) {
+            showMenu();
+            switch (storyMode) {
+                case ADVENTURE_MODE -> renderAdventureMode();
+                case ZEN_MODE -> renderZenMode();
+                case CHARACTER_MODE -> renderCharacterMode();
+                default -> System.exit(0);
+            }
+        }
+    }
+
+    private void showLoadingScreen() {
         printer.printFromFile(Dialogs.BANNER);
         printer.printCountdown(3, "\nStarting");
+    }
+
+    private void showMenu() {
         Console.clearScreen();
         printer.printFromFile(Dialogs.MENU);
 
@@ -33,5 +57,56 @@ public class Story {
         }
 
         Console.clearScreen();
+    }
+
+    private void renderAdventureMode() {
+
+    }
+
+    private void renderZenMode() {
+
+    }
+
+    private void renderCharacterMode() {
+        Console.clearScreen();
+        while (true) {
+            printCharacters();
+            int characterCode = Console.getIntegerInput("Choose a character to view information: ");
+            if (characterCode > database.getAll().size()) {
+                break;
+            }
+
+            printCharacterInformation(characterCode);
+        }
+    }
+
+    private void printCharacters() {
+        List<Character> characters = database.getAll();
+
+        System.out.println("Characters");
+        printer.printSeparator();
+        for (Character character : characters) {
+            int characterIndex = characters.indexOf(character) + 1;
+            String characterName = character.getName();
+            System.out.printf("%d. %s\n", characterIndex, characterName);
+        }
+        printer.printSeparator();
+    }
+
+    private void printCharacterInformation(int characterCode) {
+        List<Character> characters = database.getAll();
+
+        printer.printSeparator();
+        for (int i = 0; i < characters.size(); i++ ){
+            if (i == (characterCode - 1)) {
+                Character character = characters.get(i);
+                System.out.printf(
+                        "Name: %s\nAge: %s\nDescription: %s\n",
+                        character.getName(),
+                        character.getAge(),
+                        character.getDescription()
+                );
+            }
+        }
     }
 }
